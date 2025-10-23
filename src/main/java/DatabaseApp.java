1bd3d8731d48f5d247ea804b6ef2a466f062eadb
Tamara <toma_tomova@hotmail.com>
2025-10-23 09:09:07 +0200

    methode read toegevoegd

--- src/main/java/DatabaseApp.java
+++ src/main/java/DatabaseApp.java
@@ -1,9 +1,20 @@
 
+import repositories.BeerRepository;
+import repositories.BrewerRepository;
 import repositories.CategoryRepository;
 
 public class DatabaseApp {
     public static void main(String[] args) {
+        System.out.println("------ Categories -------");
         CategoryRepository categoryRepository = new CategoryRepository();
         categoryRepository.read();
+
+        System.out.println("------ Brewers -------");
+        BrewerRepository brewerRepository = new BrewerRepository();
+        brewerRepository.read();
+
+        System.out.println("------ Beers -------");
+        BeerRepository beerRepository = new BeerRepository();
+        beerRepository.read();
     }
 }
--- src/main/java/repositories/BeerRepository.java
+++ src/main/java/repositories/BeerRepository.java
@@ -1,4 +1,31 @@
 package repositories;
 
+import models.Category;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class BeerRepository {
+    public List<Category> read() {
+        List<Category> results = new ArrayList<>();
+        try {
+            Connection connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/thebelgianbrewerydb", "intec", "intec-123"
+            );
+            System.out.println("Connection to DB in made");
+            Statement statement = connection.createStatement();
+            String query = "SELECT Id, Name FROM beers";
+
+            ResultSet resultSet = statement.executeQuery(query);
+
+            while (resultSet.next()) {
+                System.out.println("ID: " + resultSet.getInt("Id"));
+                System.out.println(" Name: " + resultSet.getString("Name"));
+            }
+        } catch (SQLException sqlException) {
+            System.err.println("SQL Exeprion: " + sqlException.getMessage());
+        }
+        return results;
+    }
 }
